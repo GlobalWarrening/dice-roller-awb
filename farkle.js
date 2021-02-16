d766af0097db1731bddc390f93e48569a4a3bf23
GlobalWarrening <alexwburg@yahoo.com>
2021-02-16 10:10:53 -0600

    fixed help button, and added autofocus

--- farkle.js
+++ farkle.js
@@ -1,5 +1,23 @@
+//Alex Burgess
+//CSPS-44000
+//Functions for dice roller to play farkle
 
+function HelpButton(){
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
 
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+        });
+    }
+}
 
 function RollDice(){
     document.getElementById("dice0").value = Math.floor(Math.random()*6)+1;
@@ -10,6 +28,4 @@ function RollDice(){
     document.getElementById("dice5").value = Math.floor(Math.random()*6)+1;
 }
 
-function TotalCalc(){
 
-}
